6c9b1d414a2ac12a305acc0e392830259184e41a
Melissa Ibarra <melissa.ibarra.90@gmail.com>
2018-12-10 09:27:18 -0800

    Se agrego la funcionalidad modificar usuario

--- TestBridgeApp/urls.py
+++ TestBridgeApp/urls.py
@@ -4,6 +4,8 @@ from TestBridgeApp import views
 
 
 urlpatterns = [
+     url(r'^profile/(?P<username>\w{1,50})/$',views.user_edit, name='profile'),
      url(r'^contact$', views.contact, name='contact'),
      url(r'^', views.home, name='home'),
+
 ]
