f7d16d6eeed2df05f6456a459079f1d6ffd2671e
Melissa <44487253+MelissaIF90@users.noreply.github.com>
2018-12-26 16:10:17 -0800

    Revert "Carlos"

--- TestBridgeApp/admin.py
+++ TestBridgeApp/admin.py
@@ -2,13 +2,5 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Page
 
-
-class PageAdmin(admin.ModelAdmin):
-  list_display = ('title','update_date')
-  ordering = ('title',)
-  search_fields = ('title',)
-
-
-admin.site.register(Page, PageAdmin)
+# Register your models here.
