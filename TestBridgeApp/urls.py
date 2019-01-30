04b2382b5012a5c075ef9800364ac812d3e1f309
Carlosgthub <31784932+Carlosgthub@users.noreply.github.com>
2019-01-29 17:44:56 -0800

    Se corrigió lo de urls

--- TestBridgeApp/urls.py
+++ TestBridgeApp/urls.py
@@ -5,14 +5,11 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-     url(r'^contact', login_required(views.contact), name='contact'),
-     url(r'^home', login_required(views.home), name='home'),
-     url(r'^pricing', views.pricing, name='pricing'),
-     url(r'^resources', views.resources, name='resources'),
-     url(r'^solutions', views.testingSolutions, name='solutions'),
-     url(r'^testing', views.testing, name='testing'),
-     url(r'^', views.start, name='start'),
-
-
-
+     url(r'^contact/$', login_required(views.contact), name='contact'),
+     url(r'^home/$', login_required(views.home), name='home'),
+     url(r'^pricing/$', views.pricing, name='pricing'),
+     url(r'^resources/$', views.resources, name='resources'),
+     url(r'^solutions/$', views.testingSolutions, name='solutions'),
+     url(r'^testing/$', views.testing, name='testing'),
+     url(r'^$', views.start, name='start'),
 ]
